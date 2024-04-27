47230d52ab5774fddf18fb7b39597fca0b492d61
Pranto sheikh <prantosheikh834@gmail.com>
2024-04-27 18:44:31 +0600

    add

---
+++ file-1.js
@@ -0,0 +1,3 @@
+const { a, b, add } = require("./file-2");
+
+console.log(a, b, add(1, 3));
---
+++ file-2.js
@@ -0,0 +1,12 @@
+const a = 10;
+const b = 10;
+
+const add = (params1, params2) => params1 + params2;
+
+module.exports = {
+	a,
+	add,
+	b,
+};
+
+console.log(module);
